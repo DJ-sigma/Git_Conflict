59574bae52241f71b321322dea589b58521d7193
Debojyoti Mitra <debojyotimitra.devops@gmail.com>
2025-07-14 14:59:43 +0530

    rg_main.tf

--- child/todo_rg/rg_main.tf
+++ child/todo_rg/rg_main.tf
@@ -2,8 +2,3 @@ resource "azurerm_resource_group" "DRK" {
   name     = var.resource_group_name
   location = var.resource_group_location
 }
-
-resource "azurerm_resource_group" "koushik" {
-  name     = var.resource_group_name
-  location = var.resource_group_location
-}
