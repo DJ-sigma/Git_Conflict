4e3d4168a036c73f3aa3e28222ea8f24ed506150
Koushik Nandi <knandi020@gmail.com>
2025-07-18 11:05:30 +0530

    one rg add

--- child/todo_rg/rg_main.tf
+++ child/todo_rg/rg_main.tf
@@ -2,3 +2,9 @@ resource "azurerm_resource_group" "DRK" {
   name     = var.resource_group_name
   location = var.resource_group_location
 }
+
+
+resource "azurerm_resource_group" "koushik" {
+  name     = var.resource_group_name
+  location = var.resource_group_location
+}
